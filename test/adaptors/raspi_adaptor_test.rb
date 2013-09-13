b7e7a0d23e7df01e65880cb4336837df66b351d5
deadprogram <ron@hybridgroup.com>
2013-09-12 18:57:22 -0700

    Refactoring to shared GPIO drivers

--- test/adaptors/raspi_adaptor_test.rb
+++ test/adaptors/raspi_adaptor_test.rb
@@ -1,4 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
 require 'artoo/adaptors/raspi'
 
-# TODO: test for your adaptor goes here...
+describe Artoo::Adaptors::Raspi do
+  describe "analog GPIO" do
+    it "#digital_read"
+    it "#digital_write"
+  end
+end
