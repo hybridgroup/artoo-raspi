938cd470e8a03ab290931f2c25e562d3d167baa1
Edgar O Silva <oswaldo.silva@gmail.com>
2013-10-02 23:12:21 -0500

    Fixes pi-piper/pi-blaster typo, adds end of line to pi-blaster commands.

--- lib/pwm_pin.rb
+++ lib/pwm_pin.rb
@@ -2,23 +2,24 @@ class PwmPin
 
   attr_reader :pin_num, :pwm_file
 
-  PI_BLASTER_PATH = "/dev/pi-piper"
+  PI_BLASTER_PATH = "/dev/pi-blaster"
 
   def initialize(pin_num)
     @pin_num = pin_num
     @pwm_file = File.open("#{ PI_BLASTER_PATH }", "w")
+    #File.open("/dev/pi-blaster", "w") { |f| f.write("17=0.5\n") }
 
     pwm_write(0)
   end
 
   def pwm_write(value)
     #File.open("#{ PI_BLASTER_PATH }", "w") { |f| f.write("#{ pin_num }=#{ value }") }
-    @pwm_file.write("#{ pin_num }=#{ value }")
+    @pwm_file.write("#{ pin_num }=#{ value }\n")
     @pwm_file.flush
   end
 
   def release
-    @pwm_file.write("release #{ pin_num }")
+    @pwm_file.write("release #{ pin_num }\n")
     @pwm_file.flush
   end
 
