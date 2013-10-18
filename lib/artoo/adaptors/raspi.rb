676e93fa0b3ced8d23d896a80d0c2397797ae195
Edgar O Silva <oswaldo.silva@gmail.com>
2013-10-18 11:43:21 -0500

    Remove finalize declaration and commented lines.

--- lib/artoo/adaptors/raspi.rb
+++ lib/artoo/adaptors/raspi.rb
@@ -28,14 +28,8 @@ module Artoo
         24 => 8,
         26 => 7,
       }
-      finalizer :finalize
-      attr_reader :device, :pins, :pwm_pins, :i2c, :board_version
 
-      # Closes connection with device if connected
-      # @return [Boolean]
-      #def finalize
-      #  disconnect if connected?
-      #end
+      attr_reader :device, :pins, :pwm_pins, :i2c, :board_version
 
       # Creates a connection with device
       # @return [Boolean]
