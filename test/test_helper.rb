1d994a644ae7401e81774176228fdff1b29b9134
deadprogram <ron@hybridgroup.com>
2013-11-22 18:55:51 -0800

    Remove test mock no longer used sin PiPiper removed

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,14 +5,6 @@ require 'mocha/setup'
 
 Celluloid.logger = nil
 
-# here to be mocked, so we can run tests without having a real Raspberry Pi
-module PiPiper
-  class Pin
-    def initialize(params={})
-    end
-  end
-end
-
 MiniTest::Spec.before do
   Celluloid.shutdown
   Celluloid.boot
